df6f570eda5abdccf328e36733860b7af86e3c7c
Steven Tappert <admin@dark-it.net>
2018-06-10 19:22:06 +0200

    update setup.py with better metadata

--- setup.py
+++ setup.py
@@ -1,21 +1,41 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyFloatplane",
-    version="1.1.0",
-    author="Sammy8806",
-    author_email="mail@sammy8806.de",
-    description="Unofficial REST-Client for Floatplane.com",
+    name='pyFloatplane',
+    version='1.1.2',
+    author='Sammy8806',
+    author_email='mail@sammy8806.de',
+    description='Unofficial REST-Client bindings for Floatplane.com',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://git.dark-it.net/stappert/pyFloatplane",
+    long_description_content_type='text/markdown',
+    url='https://git.dark-it.net/stappert/pyFloatpane',
+    keywords='floatplane floatplanemedia rest-client linustechtips linusmedia videos',
+    project_urls={
+        'Floatplane': 'https://floatplane.com',
+    },
+    license='MIT',
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Multimedia :: Video',
     ),
+    install_requires=[
+        'requests',
+        'python-dateutil'
+    ],
+    python_requires='>=3.5',
+    package_data={
+        '': ['*.txt', '*.rst', '*.md'],
+    },
 )
