a653e90350791cffb282219fb67785de00889ed2
Steven Tappert <admin@dark-it.net>
2018-06-30 19:11:58 +0200

    updated edge-client infos

--- PyFloatplane/models/Client.py
+++ PyFloatplane/models/Client.py
@@ -1,13 +1,24 @@
 class Client:
-	def __init__(self):
-		self.ip = None # String : IPv4/IPv6
-		self.country_code = None # String : [US]
-		self.country_name = None # String : [United State]
-		self.region_code = None # String ?
-		self.region_name = None # String ?
-		self.city = None # String ?
-		self.zip_code = None # String ?
-		self.time_zone = None # String ?
-		self.latitude = 0.0 # Double
-		self.longitude = 0.0 # Double
-		self.metro_code = 0 # Int ?
+    def __init__(self, ip=None, country_code=None, country_name=None, region_code=None, region_name=None,
+                 city=None, zip_code=None, time_zone=None, latitude = 0.0, longitude=0.0, metro_code=0):
+        self.ip = ip  # String : IPv4/IPv6
+        self.country_code = country_code  # String : [US]
+        self.country_name = country_name  # String : [United State]
+        self.region_code = region_code  # String ?
+        self.region_name = region_name  # String ?
+        self.city = city  # String ?
+        self.zip_code = zip_code  # String ?
+        self.time_zone = time_zone  # String ?
+        self.latitude = latitude  # Double
+        self.longitude = longitude  # Double
+        self.metro_code = metro_code  # Int ?
+
+    @staticmethod
+    def generate(source):
+        if source is None or len(source) is 0:
+            return Client()
+
+        return Client(ip=source['ip'], country_code=source['country_code'], country_name=source['country_name'],
+                      region_code=source['region_code'], region_name=source['region_name'], city=source['city'],
+                      zip_code=source['zip_code'], time_zone=source['time_zone'], latitude=source['latitude'],
+                      longitude=source['longitude'], metro_code=source['metro_code'])
--- PyFloatplane/models/Edge.py
+++ PyFloatplane/models/Edge.py
@@ -1,18 +1,22 @@
+from PyFloatplane.models.Client import Client
 from PyFloatplane.models.EdgeServer import EdgeServer
 
+
 class Edge:
-	def __init__(self, client=None, edges=None):
-		self.client = None # Client
-		self.edges = [] # EdgeServer
+    def __init__(self, client=None, edges=None):
+        if client is dict:
+            client = Client.generate(client)
 
-		if edges and len(edges) > 0:
-			for edge in edges:
-				self.edges.append(EdgeServer.generate(edge))
+        self.client = client  # Client
+        self.edges = []  # EdgeServer
 
-	@staticmethod
-	def generate(source):
-		if source is None or len(source) is 0:
-			return Edge()
-		
-		return Edge(source['client'], source['edges'])
+        if edges and len(edges) > 0:
+            for edge in edges:
+                self.edges.append(EdgeServer.generate(edge))
 
+    @staticmethod
+    def generate(source):
+        if source is None or len(source) is 0:
+            return Edge()
+
+        return Edge(source['client'], source['edges'])
